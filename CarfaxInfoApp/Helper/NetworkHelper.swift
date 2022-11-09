da6eaf465fa72711e2f6150d5398761ca44dbfd5
rustemmanafov <rustemmanafov1@gmail.com>
2022-11-09 22:45:22 +0400

    updated

---
+++ CarfaxInfoApp/Helper/NetworkHelper.swift
@@ -0,0 +1,10 @@
+//
+//  NetworkHelper.swift
+//  CarfaxInfoApp
+//
+//  Created by Rustem Manafov on 09.11.22.
+//
+
+import Foundation
+
+var carCalcURL = "https://c2b-fbusiness.customs.gov.az/api/v1/dictionaries/calcAutoDuty"
